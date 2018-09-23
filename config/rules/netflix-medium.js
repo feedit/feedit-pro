2d4125c2096211bad047b4f0fe17415e8a7fa442
xdf <xudafeng@126.com>
2018-09-23 22:17:59 +0800

    add netflix

---
+++ config/rules/netflix-medium.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const path = require('path');
+const cheerio = require('cheerio');
+
+const _ = require('../../app/helper');
+
+module.exports = {
+  enable: true,
+  run: async context => {
+    const url = 'https://medium.com/feed/netfix-techblog';
+    const siteId = path.basename(__filename).replace('.js', '');
+
+    const res = await _.requestXML(url);
+    const first = res.rss.channel.item[0];
+    first.siteId = siteId;
+    first.title = first.title.$cd;
+    first.logoUrl = 'https://feedit.github.io/feedit-pro/app/public/images/netflix.jpg';
+
+    if (_.isExisted(first)) {
+      return;
+    }
+
+    const content = first.content$encoded.$cd;
+    let $ = cheerio.load(content);
+    $ = await _.translateNode(context, $);
+    await _.archiveToDir(context, $, first);
+  },
+};
