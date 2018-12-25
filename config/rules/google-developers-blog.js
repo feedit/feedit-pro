874b3ff22d502e89da5c6a49e77eb52d2674557c
xudafeng <xudafeng@126.com>
2018-12-25 16:49:21 +0800

    feat: extend to context

--- config/rules/google-developers-blog.js
+++ config/rules/google-developers-blog.js
@@ -3,30 +3,28 @@
 const path = require('path');
 const cheerio = require('cheerio');
 
-const _ = require('../../app/helper');
-
 module.exports = {
   enable: true,
-  run: async context => {
+  run: async function() {
     const url = 'http://feeds.feedburner.com/GDBcode';
     const siteId = path.basename(__filename).replace('.js', '');
 
-    const res = await _.requestXML(url);
+    const res = await this.ctx.requestXML(url);
     const first = res.feed.entry[0];
     first.pubDate = first.published;
     first.link = first.author.uri;
     first.siteId = siteId;
     first.logoUrl = 'https://feedit.github.io/feedit-pro/app/public/images/google.jpg';
 
-    if (_.isExisted(context, first)) {
+    if (this.ctx.isExisted(first)) {
       return;
     }
 
     const content = first.content;
     let $ = cheerio.load(content);
 
-    $ = await _.translateNode(context, $);
+    $ = await this.ctx.translateNode($);
 
-    await _.archiveToDir(context, $, first);
+    await this.ctx.archiveToDir($, first);
   },
 };
